d1c0c5b1b8d19c57f6e23ca007ead245498f8171
ZHAOLIBIN098 <zhaolibin098@pingan.com.cn>
2018-08-09 17:46:18 +0800

    升级springboot到2.0.4

--- src/main/java/com/my/blog/website/interceptor/WebMvcConfig.java
+++ src/main/java/com/my/blog/website/interceptor/WebMvcConfig.java
@@ -2,10 +2,8 @@ package com.my.blog.website.interceptor;
 
 
 import com.my.blog.website.utils.TaleUtils;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -13,13 +11,15 @@ import javax.annotation.Resource;
  * 向mvc中添加自定义组件
  * Created by BlueT on 2017/3/9.
  */
-@Component
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Resource
     private BaseInterceptor baseInterceptor;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(baseInterceptor);
+        registry.addInterceptor(baseInterceptor)
+                //告知拦截器：/static/admin/** 与 /static/user/** 不需要拦截 （配置的是 路径）
+                .excludePathPatterns("/static/admin/**", "/static/user/**");
     }
 
     /**
@@ -29,6 +29,8 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/upload/**").addResourceLocations("file:"+ TaleUtils.getUploadFilePath()+"upload/");
-        super.addResourceHandlers(registry);
+        //需要告知系统，这是要被当成静态文件的！
+        //第一个方法设置访问路径前缀，第二个方法设置资源路径
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 }
